f7970ca62a82cfdfa829b478a376d57d2d9ec2f9
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2021-02-23 15:00:23 +0300

    Feat: add crash-test option

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,13 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
+// Краш-тест
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 // Роутинг
 app.post('/signin', login);
 app.post('/signup', createUser);
