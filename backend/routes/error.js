f037cfcf245787669c71793c959af7ad811913fb
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2021-02-23 16:54:32 +0300

    Fixing bugs

--- backend/routes/error.js
+++ backend/routes/error.js
@@ -4,7 +4,9 @@ router.use('/*', (req, res) => {
   if (req.method === 'GET') {
     res.status(404).json({ message: 'Запрашиваемый ресурс не найден' });
   }
+/*
   res.status(404).json({ message: `Невозможно выполнить действие ${req.method} для http://localhost:3000${req.path + req.params[0]}` });
+*/
 });
 
 module.exports = router;
