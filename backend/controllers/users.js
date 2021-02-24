634b8ff19a9a742ef41ede89628315fe14388657
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2021-02-24 22:55:54 +0300

    Secure password from display

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -72,7 +72,12 @@ module.exports.createUser = (req, res, next) => {
       email,
       password: hash,
     }))
-    .then((user) => res.status(200).send({ data: user }))
+    .then((user) => res.status(200).send({
+      name: user.name,
+      about: user.about,
+      avatar: user.avatar,
+      email: user.email,
+    }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         throw new BadRequestError('Переданы некорректные данные');
